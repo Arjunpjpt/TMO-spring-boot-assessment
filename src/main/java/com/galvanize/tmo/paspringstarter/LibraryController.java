3cee62d7e61f731baab478e9f109610f4e1b2cbb
APrajap1 <arjunpjpt@gmail.com>
2022-01-26 11:06:51 -0800

    api requestmapping added

--- src/main/java/com/galvanize/tmo/paspringstarter/LibraryController.java
+++ src/main/java/com/galvanize/tmo/paspringstarter/LibraryController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ import com.galvanize.tmo.Service.LibraryService.ILibraryService;
 
 @RestController
 //@CrossOrigin(origin = "*", allowedHeaders="*")
+@RequestMapping(value="/api")
 public class LibraryController {
 
 	@Autowired
